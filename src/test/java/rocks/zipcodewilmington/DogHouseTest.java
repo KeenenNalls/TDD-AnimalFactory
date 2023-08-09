ce264ce0a2ea59c3d6334012355904fa2b0b640d
Keenen <keenen@Zipcoders-MacBook-Pro.local>
2023-08-09 15:33:06 -0400

    all test passed

--- src/test/java/rocks/zipcodewilmington/DogHouseTest.java
+++ src/test/java/rocks/zipcodewilmington/DogHouseTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import rocks.zipcodewilmington.animals.Dog;
 import rocks.zipcodewilmington.animals.animal_creation.AnimalFactory;
 import rocks.zipcodewilmington.animals.animal_storage.DogHouse;
+import org.junit.Assert;
+import org.junit.Before;
 
 import java.util.Date;
 
@@ -24,11 +26,66 @@ public class DogHouseTest {
         Date birthDate = new Date();
         Dog animal = AnimalFactory.createDog(name, birthDate);
         DogHouse.clear();
+        Integer expected = 1;
 
         // When
         DogHouse.add(animal);
+        Integer actual = DogHouse.getNumberOfDogs();
 
         // Then
-        DogHouse.getNumberOfDogs();
+        //DogHouse.getNumberOfDogs();
+        Assert.assertEquals(expected, actual);
     }
+    @Test
+    public void addDogTest(){
+        //Given
+        String givenName = "Zula";
+        Date givenBirthDate = new Date();
+        Integer givenId = 0;
+        Dog expectedDog = new Dog(givenName, givenBirthDate, givenId);
+        DogHouse.clear();
+
+        //when
+        DogHouse.add(expectedDog);
+        Dog actualDog = DogHouse.getDogById(0);
+        //then
+        Assert.assertEquals(expectedDog, actualDog);
+    }
+    @Test
+    public void removeDogTest(){
+        //Given
+        String givenName = "Zula";
+        Date givenBirthDate = new Date();
+        Integer givenId = 0;
+        Dog dogHouseDog = new Dog(givenName, givenBirthDate, givenId);
+        DogHouse.add(dogHouseDog);
+        Dog expectedDog = DogHouse.getDogById(0);
+
+        //When
+        DogHouse.remove(expectedDog);
+        Dog actualDog = DogHouse.getDogById(0);
+
+        //then
+        Assert.assertNotEquals(expectedDog, actualDog);
+    }
+    @Test
+    public void removeDogByIdTest(){
+        //Given
+        String givenName = "Zula";
+        Date givenBirthDate = new Date();
+        Integer givenId = 0;
+        Dog dogHouseDog = new Dog(givenName, givenBirthDate, givenId);
+        DogHouse.add(dogHouseDog);
+        Dog expectedDog = DogHouse.getDogById(0);
+
+        //When
+        DogHouse.remove(0);
+        Dog actualDog = DogHouse.getDogById(0);
+
+        //then
+        Assert.assertNotEquals(expectedDog, actualDog);
+    }
+
+
 }
+
